3b72b6f76422a8ecc77d0b994929ccf14d48c3b8
Yasmin Lopes <158047883+yasmin-movingpay@users.noreply.github.com>
2024-04-09 16:41:45 -0300

    get component name and directory path

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const fs = require('fs');
 const path = require('path');
 const minimist = require('minimist');
 
+// Obtendo o nome do componente e o caminho do diretório
+const args = minimist(process.argv.slice(2), {
+  string: ['props'],
+  default: {
+    props: ''
+  }
+})
+
+let directoryPath = args._[0]
+
+if (!directoryPath) {
+  console.error('❌ algo de errado não está certo. você esqueceu de passar o nome do componente ou o caminho do diretório!')
+  
+  process.exit(1)
+}
+
+let componentName = path.basename(directoryPath)
+
