0f55adee8a9c14c2148bddb4611d4c10a28aad5e
Yasmin Lopes <158047883+yasmin-movingpay@users.noreply.github.com>
2024-04-09 16:42:52 -0300

    converting camel case to kebab case

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ let componentName = path.basename(directoryPath)
 if (args.view) {
   componentName = `${componentName}View`;
 }
+
+function camelToKebab(string) {
+  let result = string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+
+  return result.charAt(0) === '-' ? result.slice(1) : result
+}
